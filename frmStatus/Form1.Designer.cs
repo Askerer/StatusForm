00cdb6a30d2b8369a4544efacddfb2f0983b010a
Asker <au3wk4@gmail.com>
2020-11-09 06:52:31 +0800

    status test

--- frmStatus/Form1.Designer.cs
+++ frmStatus/Form1.Designer.cs
@@ -28,13 +28,106 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.palbutton = new System.Windows.Forms.Panel();
+			this.btnEdit = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnCopy = new System.Windows.Forms.Button();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnDel = new System.Windows.Forms.Button();
+			this.palbutton.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// palbutton
+			// 
+			this.palbutton.Controls.Add(this.btnDel);
+			this.palbutton.Controls.Add(this.btnAdd);
+			this.palbutton.Controls.Add(this.btnCopy);
+			this.palbutton.Controls.Add(this.btnSave);
+			this.palbutton.Controls.Add(this.btnCancel);
+			this.palbutton.Controls.Add(this.btnEdit);
+			this.palbutton.Location = new System.Drawing.Point(175, 12);
+			this.palbutton.Name = "palbutton";
+			this.palbutton.Size = new System.Drawing.Size(564, 78);
+			this.palbutton.TabIndex = 0;
+			// 
+			// btnEdit
+			// 
+			this.btnEdit.Location = new System.Drawing.Point(98, 19);
+			this.btnEdit.Name = "btnEdit";
+			this.btnEdit.Size = new System.Drawing.Size(75, 40);
+			this.btnEdit.TabIndex = 0;
+			this.btnEdit.Text = "Edit";
+			this.btnEdit.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(474, 19);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 40);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(381, 19);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 40);
+			this.btnSave.TabIndex = 2;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			// 
+			// btnCopy
+			// 
+			this.btnCopy.Location = new System.Drawing.Point(190, 19);
+			this.btnCopy.Name = "btnCopy";
+			this.btnCopy.Size = new System.Drawing.Size(75, 40);
+			this.btnCopy.TabIndex = 3;
+			this.btnCopy.Text = "Copy";
+			this.btnCopy.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(7, 19);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 40);
+			this.btnAdd.TabIndex = 4;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// btnDel
+			// 
+			this.btnDel.Location = new System.Drawing.Point(285, 19);
+			this.btnDel.Name = "btnDel";
+			this.btnDel.Size = new System.Drawing.Size(75, 40);
+			this.btnDel.TabIndex = 5;
+			this.btnDel.Text = "Delete";
+			this.btnDel.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.palbutton);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.palbutton.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel palbutton;
+		private System.Windows.Forms.Button btnDel;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnCopy;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.Button btnEdit;
 	}
 }
 
--- frmStatus/Form1.cs
+++ frmStatus/Form1.cs
@@ -15,6 +15,83 @@ namespace frmStatus
 		public Form1()
 		{
 			InitializeComponent();
+
+			InitStatus();
+
+			Button btnInit = new Button();
+			btnInit.Text = "INIT";
+			btn_Status(btnInit, null);
+		}
+
+		public static string _status = "INIT";
+
+		public void ActionMode()
+		{
+			this.btnAdd.Enabled = true;
+			this.btnEdit.Enabled = true;
+			this.btnCancel.Enabled = false;
+			this.btnSave.Enabled = false;
+			this.btnDel.Enabled = true;
+			this.btnCopy.Enabled = true;
+
+		}
+		public void DesideMode()
+		{
+			this.btnAdd.Enabled = false;
+			this.btnEdit.Enabled = false;
+			this.btnCancel.Enabled = true;
+			this.btnSave.Enabled = true;
+			this.btnDel.Enabled = false;
+			this.btnCopy.Enabled = false;
+		}
+
+		public void btn_Status(object sender, EventArgs e)
+		{
+			Button btn = sender as Button;
+
+			switch (btn.Text)
+			{
+				case "Add":
+					DesideMode();
+					break;
+				case "Edit":
+					DesideMode();
+					break;
+				case "Copy":
+					DesideMode();
+					break;
+				case "Delete":
+					DesideMode();
+					break;
+				case "Save":
+					ActionMode();
+					break;
+				case "Cancel":
+					ActionMode();
+					break;
+				default:
+					ActionMode();
+					break;
+
+			}
+
+		}
+
+
+		public void InitStatus()
+		{
+			this.btnAdd.Click += new System.EventHandler(btn_Status);
+			this.btnEdit.Click += new System.EventHandler(btn_Status);
+			this.btnSave.Click += new System.EventHandler(btn_Status);
+			this.btnCopy.Click += new System.EventHandler(btn_Status);
+			this.btnCancel.Click += new System.EventHandler(btn_Status);
+			this.btnDel.Click += new System.EventHandler(btn_Status);
+		}
+
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			
 		}
 	}
 }
